0a8af02ffac3b555601d58b4eaf583bccfc9c816
itxbasit <itxelegantmansoori@gmail.com>
2026-06-11 22:35:32 +0500

    Refactor pagination logic and enhance product fetching functionality

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let list = document.getElementById("list");
 let pagination = document.getElementById("pagination");
 
-
 async function fetchApi(skip = 0, limit = 15) {
   try {
     let response = await fetch(
@@ -9,14 +8,22 @@ async function fetchApi(skip = 0, limit = 15) {
     );
     let data = await response.json();
     fetchProducts(data);
-    flag = true
+    flag = true;
   } catch (error) {
     console.log("Error fetching data:", error);
   }
 }
 
-function createPagination(totalPage, skip) {
-  let paginationHTML = `<li onclick="console.log(${skip})">
+function createPagination(totalPage, skip, currPage) {
+  let paginationHTML =
+    skip == 0
+      ? `<li>
+      <a  class="flex items-center justify-center text-gray-300 border border-gray-300 hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-s-base text-sm w-9 h-9 focus:outline-none">
+        <span class="sr-only">Previous</span>
+        <svg class="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7"/></svg>
+      </a>
+    </li>`
+      : `<li onclick="fetchApi(${skip - 15})">
       <a  class="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-s-base text-sm w-9 h-9 focus:outline-none">
         <span class="sr-only">Previous</span>
         <svg class="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m15 19-7-7 7-7"/></svg>
@@ -25,12 +32,22 @@ function createPagination(totalPage, skip) {
 
   for (let i = 1; i <= totalPage; i++) {
     paginationHTML += `<li onclick="fetchApi(${(i - 1) * 15})">
-        <a  class="flex items   -center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium text-sm w-9 h-9 focus:outline-none">${i}</a>
+        <a class="flex items-center justify-center ${
+          currPage == i ? "bg-green-400 text-white" : "text-gray-300"
+        }  box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium text-sm w-9 h-9 focus:outline-none">${i}</a>
       </li>`;
   }
 
-  paginationHTML += `<li>
-      <a  class="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-e-base text-sm w-9 h-9 focus:outline-none">
+  paginationHTML +=
+    currPage == totalPage
+      ? `<li>
+      <a  class="flex items-center justify-center text-body bg-neutral-secondary-medium text-gray-300 border border-gray-300 border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-e-base text-sm w-9 h-9 focus:outline-none">
+        <span class="sr-only">Next</span>
+        <svg class="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7"/></svg>
+      </a>
+    </li>`
+      : `<li>
+      <a onclick="fetchApi(${skip + 15})" class="flex items-center justify-center text-body bg-neutral-secondary-medium box-border border border-default-medium hover:bg-neutral-tertiary-medium hover:text-heading font-medium rounded-e-base text-sm w-9 h-9 focus:outline-none">
         <span class="sr-only">Next</span>
         <svg class="w-4 h-4 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m9 5 7 7-7 7"/></svg>
       </a>
@@ -39,11 +56,12 @@ function createPagination(totalPage, skip) {
 }
 
 function fetchProducts(data) {
-  let totalPage = Math.ceil(data.total / data.limit);
-  createPagination(totalPage, data.skip);
+  let totalPage = Math.ceil(data.total / 15);
+  let currPage = data.skip / 15 + 1;
+  createPagination(totalPage, data.skip, currPage);
   list.innerHTML = "";
   data.products.forEach((product) => {
-    list.innerHTML += `<div onclick="window.location.href='detail.html?productId=${product.id}'"
+    list.innerHTML += `<div onclick="window.location.href='detail.html?productId=${product.id}&title=${product.title}'"
         class="bg-neutral-primary-soft block w-full lg:max-w-sm p-6 border-0 rounded-2xl shadow-xl"
       >
         <a href="#">
@@ -95,4 +113,6 @@ function fetchProducts(data) {
   });
 }
 
-fetchApi()
+if(window.location.pathname == "/index.html") fetchApi();
+
+let params = new UrlSea(window.location.search)
